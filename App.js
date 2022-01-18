b3c7e1046781941f4c25143d04155cd8b3d83502
imlsw96 <a787574@naver.com>
2022-01-19 00:54:43 +0900

    2.2 use preload

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import AppLoading from "expo-app-loading";
+import { Text, Image } from "react-native";
+import * as Font from 'expo-font';
+import {Ionicons} from '@expo/vector-icons'
+import { Asset } from 'expo-asset';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [ready, setReady] = useState(false);
+  const onFinish = () => setReady(true);
+  const startLoading = async () => {
+    await Font.loadAsync(Ionicons.font);
+    await Asset.loadAsync(requiredI('./images/어빙.jpeg'));
+    await Image.prefetch("https://cdn.rookie.co.kr/news/photo/202102/57006_58898_5351.jpg")
+    
+  };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!ready) {
+    return (
+      <AppLoading
+        startAsync={startLoading}
+        onFinish={onFinish}
+        onError={console.log}
+      />
+    );
+  }
+
+  return <Text>We are done loading!</Text>;
+}
