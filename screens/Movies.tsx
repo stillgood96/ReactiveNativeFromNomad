3bf995b8abee0664fb6c30f87b5dadde08d7ff0c
imlsw96 <a787574@naver.com>
2022-02-01 18:03:23 +0900

    #3.4 Movies Screen part Three (14:10)

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,8 +1,8 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect, useState } from "react";
 import styled from "styled-components/native";
-import Swiper from 'react-native-web-swiper';
-import {ActivityIndicator, Dimensions, StyleSheet} from "react-native";
+import Swiper from 'react-native-swiper';
+import {ActivityIndicator, Dimensions, StyleSheet, useColorScheme} from "react-native";
 import { makeImagePath } from "../utils";
 import { BlurView } from 'expo-blur';
 
@@ -31,9 +31,41 @@ const BgImg = styled.Image`
     position : absolute;
 `;
 
-const Title = styled.Text``;
+const Poster = styled.Image`
+    width:100px;
+    height:160px;
+    border-radius: 5px;
+`;
+
+const Title = styled.Text`
+    font-size: 16px;
+    font-weight: 600;
+    color: white;
+`;
+
+const Wrapper = styled.View`
+    flex-direction : row;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+`;
+const Column = styled.View`
+    width: 40%;
+    margin-left:15px;
+`;
+
+const Overview = styled.Text`
+    margin-top: 10px;
+    color: rgba(255,255,255, 0.6);
+`;
+
+const Votes = styled(Overview)`
+    font-size:12px;
+`;
 
 const Movies : React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
+
+    const isDark = useColorScheme() === "dark";
 
     const [loading, setLoading] = useState(true);
     const [nowPlaying, setNowPlaying] = useState([]);
@@ -56,19 +88,32 @@ const Movies : React.FC<NativeStackScreenProps<any, 'Movies'>> = () => {
         </Loader>
     ) :  (
         <Container>
-            <Swiper loop 
-                timeout={3.45} 
-                controlsEnabled ={false}
+            <Swiper 
+                horizontal
+                loop 
+                autoplay
+                autoplayTimeout={3.45} 
+                showsButtons ={false}
+                showsPagination={false}
                 containerStyle={{width:"100%" , height: SCREEN_HEGIHT /4}}>
 
                 {nowPlaying.map(movie => <View key={movie.id}> 
                     <BgImg source={{uri: makeImagePath(movie.backdrop_path)}}
                            style= {StyleSheet.absoluteFill}/>
                     <BlurView 
+                        tint ={isDark? "dark" : "light"}
                         intensity ={80}
                         style= {StyleSheet.absoluteFill}>
-                        <Title>{movie.original_title}</Title>                        
-                    </BlurView>
+
+                        <Wrapper>
+                            <Poster source={{uri: makeImagePath(movie.poster_path)}} />
+                            <Column>
+                                <Title>{movie.original_title}</Title>    
+                                {movie.vote_average > 0 ? <Votes>⭐️{movie.vote_average}/10</Votes> : null}
+                                <Overview>{movie.overview.slice(0, 90)}...</Overview>                                 
+                            </Column>                            
+                        </Wrapper>
+                    </BlurView>                    
                 </View>)}
             </Swiper>
         </Container>
